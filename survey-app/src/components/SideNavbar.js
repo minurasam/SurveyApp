e44d815254bff5d32f486345619260d9cc611264
minna123-cap <minura.samaranayake@gmail.com>
2020-09-01 11:52:03 +0530

    added Navbar

--- survey-app/src/components/SideNavbar.js
+++ survey-app/src/components/SideNavbar.js
@@ -1,7 +1,42 @@
-import React from 'react';
-import { MDBIcon, MDBSideNavCat, MDBSideNavNav, MDBSideNav, MDBSideNavLink, MDBContainer, MDBRow, MDBBtn } from 'mdbreact';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import ReactNavbar from "react-responsive-animate-navbar";
 
 class SideNavPage extends React.Component {
-  
+    render() {
+      return (
+        <ReactNavbar
+          color="rgb(25, 25, 25)"
+          logo="https://svgshare.com/i/KHh.svg"
+          menu={[
+            { name: "HOME", to: "/" },
+            { name: "ARTICLES", to: "/articles" },
+            { name: "ABOUT ME", to: "/about" },
+            { name: "CONTACT", to: "/contact" },
+          ]}
+          social={[
+            {
+              name: "Linkedin",
+              url: "https://www.linkedin.com/in/nazeh-taha/",
+              icon: ["fab", "linkedin-in"],
+            },
+            {
+              name: "Facebook",
+              url: "https://www.facebook.com/nazeh200/",
+              icon: ["fab", "facebook-f"],
+            },
+            {
+              name: "Instagram",
+              url: "https://www.instagram.com/nazeh_taha/",
+              icon: ["fab", "instagram"],
+            },
+            {
+              name: "Twitter",
+              url: "http://nazehtaha.herokuapp.com/",
+              icon: ["fab", "twitter"],
+            },
+          ]}
+        />
+      );  
+  }
+}
 export default SideNavPage;
