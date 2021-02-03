c65247a5df5ffc390c6a19fca160ae5cf5162b76
minna123-cap <minura.samaranayake@gmail.com>
2021-02-03 14:34:44 +0530

    dockerized build 1

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ mongoose.connect(uri, {
     useCreateIndex: true, 
     useFindAndModify: true
 });
-
+ 
 const connection = mongoose.connection;
 connection.once('open', () => {
     console.log("MongoDB Database Connection is Established Succesfully!");
