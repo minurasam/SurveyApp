5659920b11f6e82b16eeaf7c48551a0c850dc90d
minna123-cap <minura.samaranayake@gmail.com>
2021-04-03 01:20:22 +0530

    Deployment Config

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,10 @@ connection.once('open', () => {
 })
 
 
+if (process.env.NODE_ENV== "production"){
+    app.use(express.static("client/build");)
+}
+
 
 app.listen(port, () => {
     console.log(`Server is running in port: ${port}`);
